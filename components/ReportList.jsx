ca0446f03e0fae22b3db1e5b2ec060d10788746a
amaniq88 <amani.musallamq88@gmail.com>
2022-07-04 09:53:37 +0300

    add more reports

--- components/ReportList.jsx
+++ components/ReportList.jsx
@@ -17,7 +17,30 @@ import { Button } from 'bootstrap';
 
 
 function ReportList() {
-const date = new Date();
+
+
+  var date1= new Date().toISOString().slice(0,10)
+
+
+  const [retrievedUser, setretrievedUser] = useState([]);
+  const getUser = async () => {
+    try {
+      const resUser = await http.get("/users", auth.config);
+      setretrievedUser(resUser.data);
+      } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = dd + '/' + mm + '/' + yyyy;
 
 const data = [{no:1,report:"Summary for whole day"},
                    {no:2,report:"Filter By name"},
@@ -36,27 +59,66 @@ const Analysis = [{no:1,report:"Average Time of complain"},
     try {
       const res = await http.get("/tasks-list", auth.config);
       setRetrievedTasks(res.data);
-      console.log(res.data);
       } catch (error) {
       console.log(error);
     }
   };
-
   useEffect(() => {
     getTasks();
   }, []);
 
-  const filtered = retrievedTasks.filter(employee => {
-    return employee.status === 'open';
+  const [Datevalue, setDateValue] = useState(date1);
+  const  formatedDay =  Datevalue
+const handleDateChange = (e) => {
+  setDateValue(Datevalue = e.target.value);
+  //2022-07-13
+  var dd = Datevalue.slice(8,10)
+  var mm = Datevalue.slice(5,7)
+  var yyyy = Datevalue.slice(0,4)
+  formatedDay  = dd + '/' + mm + '/' + yyyy;
+console.log(formatedDay)
+};
+
+  // Filter Function 
+  const filtered = retrievedTasks.filter(task => {
+    console.log(task.date);
+    return task.date === formatedDay ;
   });
-  const handleExport = () =>{
+  const handleExportReport1 = () =>{
+    console.log(filtered)
     XLSX = require('xlsx');
     var wb = XLSX.utils.book_new(),
     ws = XLSX.utils.json_to_sheet(filtered)
-
     XLSX.utils.book_append_sheet(wb,ws,"Report");
     XLSX.writeFile(wb,"Reports.xlsx");
 };
+
+
+
+const getInitialState = () => {
+  const value = "" ;
+  return value;
+};
+
+const [value, setValue] = useState(getInitialState);
+const handleChange = (e) => {
+  setValue(e.target.date1);
+};
+
+
+const filteredUser = retrievedTasks.filter(employee => {
+  return employee.assign_to === parseInt(value);
+});
+const handleExportReport2 = () =>{
+  XLSX = require('xlsx');
+  var wb = XLSX.utils.book_new(),
+  ws = XLSX.utils.json_to_sheet(filteredUser)
+  XLSX.utils.book_append_sheet(wb,ws,"Report");
+  XLSX.writeFile(wb,"Reports.xlsx");
+};
+
+
+
 
 
 
@@ -67,23 +129,45 @@ return (
   <legend className="md:text-3xl pb-8 text-[#F2F2F2] py-5">
   Reports
   </legend>
-    
+  <div  className="w-1/2  text-black mb-6 ">
+    <form>
+    <label className="w-1/2 pr-4 text-lg text-white "> Staff </label>
+                      <select onChange={handleChange} >
+                        <option
+                          className="pr-4 text-lg text-white "
+                          value=" "
+                        />
+                        {retrievedUser.map((user) => (
+                          <option key={user.id} value={user.id}>
+                            {`${user.first_name} ${user.last_name}`}
+                          </option>
+                        ))}
+                      </select>
+    <label className=" ml-4 w-1/2 pr-4 text-lg text-white " > Date</label>
+    <input type="date" onChange={handleDateChange}/>    
+    </form>
+  </div>
 	<table className="w-1/2 border-collapse text-white border-separate border border-slate-400">
     <thead>
       <tr>
-        <td className="border border-slate-300  ">no.</td>
-        <td className="border border-slate-300  ">Report Name </td>
-        <td className="border border-slate-300  ">Date</td>
-        <td className="border border-slate-300  ">download</td>
+        <td className="border border-slate-300  px-4">no.</td>
+        <td className="border border-slate-300   px-4 ">Report Name </td>
+        <td className="border border-slate-300  px-4 ">download</td>
       </tr>
     </thead>
     <tbody>
     <tr>
         <td className="border border-slate-300 px-4"> 1.</td>
         <td className="border border-slate-300 px-4">Summary for whole day</td>
-        <td className="border border-slate-300 px-4" ><Moment format='MMMM Do YYYY'>{date}</Moment></td>
         <td className="border border-slate-300 px-4" > 
-        <button  onClick={handleExport}>Export</button>
+        <button  onClick={handleExportReport1}>Export</button>
+        </td>
+    </tr>
+    <tr>
+        <td className="border border-slate-300 px-4"> 2.</td>
+        <td className="border border-slate-300 px-4">Filter By name</td>
+        <td className="border border-slate-300 px-4" > 
+        <button   onClick={handleExportReport2}>Export</button>
         </td>
     </tr>
     </tbody>    
